99cfcaebb99e4abc7a0be618ad6982c822a64e79
tdiccccc <the.earth3121@gmail.com>
2022-05-20 08:36:06 +0000

    [Add admin/workers]

--- app/controllers/admin/workers_controller.rb
+++ app/controllers/admin/workers_controller.rb
@@ -1,10 +1,28 @@
 class Admin::WorkersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @workers = Worker.all
   end
 
   def show
+    @worker = Worker.find(params[:id])
   end
 
   def edit
+    @worker = Worker.find(params[:id])
+  end
+
+  def update
+    @worker = Worker.find(params[:id])
+    @worker.update(workers_params)
+    flash[:notice] = "従業員情報を更新しました。"
+    redirect_to admin_worker_path(@worker)
+  end
+
+  private
+
+  def workers_params
+    params.require(:worker).permit(:first_name, :last_name, :email, :is_deleted)
   end
 end
