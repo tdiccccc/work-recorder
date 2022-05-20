2adfb88faa3bb98e91ca31b140a9151b6f7ffb88
tdiccccc <the.earth3121@gmail.com>
2022-05-20 08:58:11 +0000

    [Add worker/product]

--- app/controllers/worker/registrations_controller.rb
+++ app/controllers/worker/registrations_controller.rb
@@ -3,7 +3,17 @@
 class Worker::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    progresses_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
+  end
   # GET /resource/sign_up
   # def new
   #   super
